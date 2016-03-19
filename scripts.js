ded398f9d9531cd5bfe589a17261e204e0af1b83
oberon <pnaxighn+gh@gmail.com>
2016-03-19 15:34:57 -0400

    Save/load, first-chance labeling, better remainders, better root-rendering.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,67 @@
+var SavedBuilds = {};
+function InitializeBuilds()
+{
+	var stored = localStorage.getItem("builds");
+	if ( stored )
+		SavedBuilds = JSON.parse(stored);
+	else
+		SavedBuilds = {};
+}
+
+function GetAllBuilds()
+{
+	var bs = [];
+	for ( var b in SavedBuilds )
+		bs.push(b);
+	return bs;
+}
+
+function LoadBuild(n)
+{
+	var b = SavedBuilds[n];
+	included = {};
+	for ( var k in b.included )
+		included[k] = b.included[k];
+}
+
+function SaveBuild(n)
+{
+	var b = { included:{} };
+	for ( var k in included )
+		b.included[k] = included[k];
+	SavedBuilds[n] = b;
+	localStorage.setItem("builds", JSON.stringify(SavedBuilds));
+}
+
+var included = {};
+
+function Clear(n)
+{
+	while (n.childNodes.length > 0)
+		n.removeChild(n.childNodes[0]);
+}
+
+function SetIncluded(constellation, value)
+{
+	included[constellation.Name] = value;
+}
+
+function GetIncluded(constellation)
+{
+	var cs = [];
+	for ( var maybe in included )
+	{
+		if ( included[maybe] )
+			cs.push(objByName[maybe]);
+	}
+	return cs;
+}
+
+function SwitchConf()
+{
+	var c = document.getElementById("conf");
+	conf.className = conf.className == "confOpen" ? "confClose" : "confOpen";
+}
 
 function RenderGraph(els)
 {
@@ -12,15 +76,15 @@ var cy = cytoscape({
     {
       selector: 'node',
       css: {
-        'content': 'data(pointsLeft)',
+        'content': 'data(first)',
         'text-valign': 'center',
         'text-halign': 'center',
-        'font-size': 6,
+        'font-size': 3,
         'width':15,
         'height':15,
-        'opacity':0.4,
+        'background-opacity':0.4,
         'background-color':'#0000aa',
-        'color':'#000000'
+        'color':'#ff0000'
       }
     },
     {
